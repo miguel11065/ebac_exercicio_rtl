6d9f4dcb459c3e7ced8ec57eecf76d5ba4b13246
Miguel Leal <miguel11065@gmail.com>
2023-10-05 16:29:36 -0300

    código para testes

--- src/components/PostComments/PostComments.test.tsx
+++ src/components/PostComments/PostComments.test.tsx
@@ -1,10 +1,29 @@
 import { fireEvent, render, screen } from '@testing-library/react';
-import Post from '.';
+// import Post from '.';
 import PostComment from '.';
 
 describe('Teste para o componente PostComment', () => {
     it('Deve renderizar o componente corretamente', () => {
         render(<PostComment/>);
         expect(screen.getByText('Comentar')).toBeInTheDocument();
-    });
-});
+    })
+
+    test('Deve adcionar dois comentarios', () => {
+        render(<PostComment />)
+        fireEvent.change(screen.getByTestId('campo-comentario'), {
+            target: {
+                value: 'Primeiro comentario de teste'
+            }
+        })
+        fireEvent.click(screen.getByTestId('btn-comentar'))
+
+        fireEvent.change(screen.getByTestId('campo-comentario'), {
+            target: {
+                value: 'Segundo comentario de teste'
+            }
+        })
+        fireEvent.click(screen.getByTestId('btn-comentar'))
+
+        expect(screen.getAllByTestId('comentario-de-teste')).toHaveLength(2)
+    })
+})
